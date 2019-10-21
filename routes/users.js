e179007c68994fd0800b467174bc12ff62ad0c7a
g-alcorn <griffin.alcorn@gmail.com>
2019-10-21 16:09:25 +0000

    routes for all user-profile-related functions

--- routes/users.js
+++ routes/users.js
@@ -9,29 +9,48 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+  //LOGIN BUTTON PRESS - SHOW FORM
+  router.get('login', (req, res) => {
+    //RENDER FORM WITH AJAX
+  });
+
+  //LOGIN BUTTON PRESS - AUTHENTICATE
   router.post("/login", (req, res) => {
     db.query(`SELECT * FROM users;`)
       .then(data => {
+        //CALL AUTHENTICATION FUNCTION FROM HELPER FILE
         const users = data.rows;
         res.json({ users });
       })
       .catch(err => {
+        //CREATE ERROR CODE
         res
           .status(500)
           .json({ error: err.message });
       });
   });
 
+  //LOGOUT BUTTON PRESS
   router.post('/logout', (req, res) => {
     //delete cookies
     //change top bar to logged out appearance
   });
 
+  //REGISTER BUTTON PRESS - SHOW FORM
+  router.get('/register'), (req, res) => {
+    //RENDER FORM WITH AJAX
+  }
+
+  //REGISTER BUTTON PRESS - AUTHENTICATE
   router.post('/register', (req, res) => {
-    //validate form data
+    //validate form data with helper function
     //save to DB
-    //create cookies and convert to login view
+    //create cookies and convert to logged-in view
   });
 
+  //GET USER PROFILE
+  router.get('/:id', (req, res) => {
+    //RENDER LIST OF ALL MAPS MADE BY A USER
+  })
   return router;
 };
