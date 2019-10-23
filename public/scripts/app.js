e4b4c0291abee248d70fea9a91405e61a8cc9fd0
g-alcorn <griffin.alcorn@gmail.com>
2019-10-23 14:37:22 +0000

    bubbles appear on load

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,6 +28,8 @@ $(document).ready(function() {
     $('#mapid').removeClass('open')
     $('main').removeClass('open')
   })
+
+
   //CHANGE JQUERY SELECTOR TO MATCH NEW/EDIT MAP BUTTONS
   $('form-submit').click(function(event) {
     event.preventDefault();
@@ -41,11 +43,11 @@ $(document).ready(function() {
     });
   });
 
-  //MAP CLICK
-  map.on('click', function(event) {
-    console.log('trying to load a bubble');
-    onMapClick(event, map);
-  });
+  //MAP CLICK TESTING
+  // map.on('click', function(event) {
+  //   console.log('trying to load a bubble');
+  //   onMapClick(event, map);
+  // });
 
 });
 
@@ -57,6 +59,62 @@ const initMap = (map) => {
   const background = new L.TileLayer(osmUrl, {attribution: osmAttrib});
 
   map.addLayer(background);
+
+  //add test geojson
+  const myFile = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.57818603515624,
+            45.5414651417455
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.61320495605469,
+            45.43074435456533
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.72787475585938,
+            45.51212126820532
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.56651306152344,
+            45.49912810913339
+          ]
+        }
+      }
+    ]
+  };
+
+  const myLayer = L.geoJSON(myFile, {onEachFeature: onEachFeature}).addTo(map);
+  //myLayer.addData(myFile);
+
+  //onEachFeature(myFile, myLayer);
 };
 
 //OPEN POPUP WITH LATLNG
@@ -67,3 +125,16 @@ const onMapClick = (e, map) => {
     .setContent("You clicked the map at " + e.latlng.toString());
   map.openPopup(popup);
 };
+
+
+const onEachFeature = (feature, layer) => {
+  var popupContent = "<p>I started out as a GeoJSON " +
+      feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
+
+  if (feature.properties && feature.properties.popupContent) {
+    popupContent += feature.properties.popupContent;
+  }
+
+  layer.bindPopup(popupContent);
+}
+
