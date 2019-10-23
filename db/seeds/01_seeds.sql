34b171495a493483a219d29cd9e3e9feb17ec159
g-alcorn <griffin.alcorn@gmail.com>
2019-10-23 14:36:29 +0000

    seed data for testing

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,11 @@
+-- Users table seeds here (Example)
+INSERT INTO users (name, email, password, account_created)
+VALUES
+('Olivier Carignan', 'olive.carignan@gmail.com', '123', now()),
+('Griffin Alcorn', 'griffin.alcorn@gmail.com', '123', now()),
+('Lighthouse', 'labber@lighthouselabs.com', '123', now());
+
+INSERT INTO maps (user_id, title, description, location)
+VALUES
+('1', 'My First Map', 'this is my first map to test our app', '../../lib/testMap.geojson'),
+('2', 'My Second Map', 'this is another test', '../../lib/testMap.geojson');
